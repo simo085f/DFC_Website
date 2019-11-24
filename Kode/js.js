324488be63ee0fdeefe0c6104e4cd983cf703269
Simone Grauer <sgr@selectedmedia.dk>
2019-11-24 21:19:51 +0100

    Opbygning af index.html i mobil

---
+++ Kode/js.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function aabenNav() {
+  document.querySelector("#mobilNavigation").style.width = "100%";
+}
+
+function lukNav() {
+  document.querySelector("#mobilNavigation").style.width = "0%";
+}
