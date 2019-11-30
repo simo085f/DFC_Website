3ecf339d23bc2a8ce76deeabb516bc44834072ed
Simone Grauer <sgr@selectedmedia.dk>
2019-11-30 20:35:18 +0100

    test med scrollreveal

---
+++ Kode/scroll.js
@@ -0,0 +1,23 @@
+ScrollReveal().reveal(
+  ".produkt1Container, .produkt2Container, .produkt3Container, .produkt4Container, .produkt5Container",
+  {
+    viewFactor: 1000,
+    delay: 200,
+    easing: "ease-in"
+  }
+);
+
+/* ScrollReveal().reveal(".produkt2Container", {
+  viewFactor: 1000,
+  delay: 300
+});
+
+ScrollReveal().reveal(".produkt3Container", {
+  viewFactor: 1000,
+  delay: 300
+});
+
+ScrollReveal().reveal(".produkt4Container", {
+  viewFactor: 1000,
+  delay: 300
+}); */
