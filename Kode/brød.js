3676a80b4694303f63e8726a1d9e6c29c1d6b4fe
Simone Grauer <sgr@selectedmedia.dk>
2019-11-30 20:06:02 +0100

    Scrollreveal på brød.html

---
+++ Kode/brød.js
@@ -0,0 +1,15 @@
+ScrollReveal().reveal(".produkt1Container", {
+  viewFactor: 1000
+});
+
+ScrollReveal().reveal(".produkt2Container", {
+  viewFactor: 1000
+});
+
+ScrollReveal().reveal(".produkt3Container", {
+  viewFactor: 1000
+});
+
+ScrollReveal().reveal(".produkt4Container", {
+  viewFactor: 1000
+});
