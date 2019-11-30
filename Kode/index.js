28b146e98da46d261efb1c7ffc2ef79b11934a77
Simone Grauer <sgr@selectedmedia.dk>
2019-11-30 19:01:58 +0100

    Forsøg med scrollreveal på txt elementer

---
+++ Kode/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+ScrollReveal().reveal(".borderTxtContainer", {
+  viewFactor: 1000
+  /* distance: "800px",
+  origin: "right",
+  duration: 1000 */
+});
+
+ScrollReveal().reveal(".kagensDagContainer", {
+  viewFactor: 1000
+  /* distance: "800px",
+    origin: "right",
+    duration: 1000 */
+});
