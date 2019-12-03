b3fa96aa122b9109bd07ee46884c97727af780e6
Simone Grauer <sgr@selectedmedia.dk>
2019-12-03 21:35:27 +0100

    test med media queries på scrollreveal

--- Kode/scroll.js
+++ Kode/scroll.js
@@ -1,5 +1,7 @@
 "use strict";
-
+let width = window.matchMedia("(max-width: 768px)");
+ScrollFunction(width);
+width.addListener(ScrollFunction);
 ScrollReveal().reveal(".borderTxtContainer", {
   viewFactor: 1.0,
   delay: 100,
@@ -12,23 +14,42 @@ ScrollReveal().reveal(".kagensDagContainer", {
   easing: "ease-in"
 });
 
-ScrollReveal().reveal(
-  ".produkt1Container, .produkt2Container, .produkt3Container, .produkt4Container",
-  {
-    viewFactor: 1000,
-    delay: 200,
-    easing: "ease-in"
+function ScrollFunction() {
+  if (width.matches) {
+    ScrollReveal().reveal(
+      ".produkt1Container, .produkt2Container, .produkt3Container, .produkt4Container",
+      {
+        viewFactor: 1.0,
+        delay: 200,
+        easing: "ease-in"
+      }
+    );
+  } else {
+    ScrollReveal().reveal(
+      ".produkt1Container, .produkt2Container, .produkt3Container, .produkt4Container",
+      {
+        viewFactor: 0.8,
+        delay: 200,
+        easing: "ease-in"
+      }
+    );
   }
-);
+}
 
 ScrollReveal().reveal(".kontaktTxtContainer", {
-  viewFactor: 1.0,
+  viewFactor: 0.8,
   delay: 200,
   easing: "ease-in"
 });
 
-ScrollReveal().reveal(".aarstal, .aarstalDesktop", {
-  viewFactor: 1.0,
-  delay: 100,
+ScrollReveal().reveal(".aarstal", {
+  viewFactor: 0.8,
+  delay: 200,
+  easing: "ease-in"
+});
+
+ScrollReveal().reveal(".aarstalDesktop", {
+  viewFactor: 0.6,
+  delay: 200,
   easing: "ease-in"
 });
