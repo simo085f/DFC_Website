932af3cc8c678487081a3fb8715adab311f203a8
Simone Grauer <sgr@selectedmedia.dk>
2019-12-03 21:00:49 +0100

    index.js er rykket ind i scroll.js så alt scrollreveal er samlet

--- Kode/scroll.js
+++ Kode/scroll.js
@@ -1,3 +1,17 @@
+"use strict";
+
+ScrollReveal().reveal(".borderTxtContainer", {
+  viewFactor: 1.0,
+  delay: 100,
+  easing: "ease-in"
+});
+
+ScrollReveal().reveal(".kagensDagContainer", {
+  viewFactor: 0.8,
+  delay: 100,
+  easing: "ease-in"
+});
+
 ScrollReveal().reveal(
   ".produkt1Container, .produkt2Container, .produkt3Container, .produkt4Container",
   {
