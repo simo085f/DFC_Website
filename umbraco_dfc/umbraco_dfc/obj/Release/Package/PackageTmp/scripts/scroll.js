27d51754ecf95eec2b5c58b61122d43e04a51196
Simone Grauer <sgr@selectedmedia.dk>
2019-12-09 15:01:09 +0100

    deploy

---
+++ umbraco_dfc/umbraco_dfc/obj/Release/Package/PackageTmp/scripts/scroll.js
@@ -0,0 +1,56 @@
+"use strict";
+let width = window.matchMedia("(max-width: 768px)");
+ScrollFunction(width);
+width.addListener(ScrollFunction);
+ScrollReveal().reveal(".borderTxtContainer", {
+  viewFactor: 1.0,
+  delay: 100,
+  easing: "ease-in"
+});
+
+ScrollReveal().reveal(".kagensDagContainer", {
+  viewFactor: 0.5,
+  delay: 200,
+  easing: "ease-in"
+});
+
+function ScrollFunction() {
+  if (width.matches) {
+    ScrollReveal().reveal(
+      ".produkt1Container, .produkt2Container, .produkt3Container, .produkt4Container",
+      {
+        viewFactor: 1.0,
+        delay: 200,
+        easing: "ease-in"
+      }
+    );
+  } else {
+    ScrollReveal().reveal(
+      ".produkt1Container, .produkt2Container, .produkt3Container, .produkt4Container",
+      {
+        viewFactor: 0.7,
+        delay: 150,
+        easing: "ease-in"
+      }
+    );
+  }
+}
+
+ScrollReveal().reveal(".kontaktTxtContainer", {
+  viewFactor: 0.8,
+  delay: 200,
+  easing: "ease-in"
+});
+
+ScrollReveal().reveal(".aarstal", {
+  viewFactor: 0.3,
+  easing: "ease-in"
+});
+
+ScrollReveal().reveal(".aarstalDesktop", {
+  viewFactor: 0.6,
+  delay: 100,
+  easing: "ease-in"
+});
+
+                
